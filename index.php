98d2df6d1566964059f388a1d1eb59410da61b59
Syed <srgithubsr@gmail.com>
2024-11-01 10:10:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,136 @@
+<?php include 'header.php';?>
+<!-- Menu Sidebar Section Start -->
+<div class="body-overlay"></div>
+<!-- Slider Area Start  !-->
+<div class="slider-area style-1 bg-icon res-slider">
+    <div class="shape-one">
+        <!-- <img src="images/shape/triangle.png" alt="shape"> -->
+    </div>
+    <div class="slider-wrapper">
+        <!-- single slider start !-->
+        <div class="single-slider-wrapper">
+            <div class="single-slider">
+                <div class="container h-100 align-self-center">
+                    <div class="row h-100 justify-content-center">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- single slider end !-->
+    </div>
+</div>
+<!-- Slider Area End !-->
+<!--why-choose-area  Start !-->
+<div class="why-choose-area style-1 py-120 py-sm-60 py-md-80 py-lg-100 overflow-hidden"
+    style="background-color: white;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-xl-5 wow animate__animated animate__jackInTheBox animate__slow">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/about-us/shri-varaham-swastik-statue.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 p-0 wow animate__animated animate__fadeInRight animate__slow">
+                <div class="about-info-card">
+                    <div class="section-title">
+                        <div class="short-title">
+                            <span>About Us</span>
+                        </div>
+                    </div>
+                    <div class="list-item-wrapper">
+                        <ul>
+                            <li style="justify-content: left;">Providing basic necessities to the needy poor people.
+                            </li>
+                            <li>To provide food and shelter to the socially backward; sadhus; pakkirs etc and to
+                                meet there medical and other needs for daily living. </li>
+                            <li>To construct and maintain Hindu Temple for Public Workship under the control of
+                                Trust. </li>
+                            <li>To construct Traditional Indian medicine Hospital for providing medical relief of
+                                public at large. </li>
+                        </ul><br>
+                        <div>
+                            <a href="#" class="theme-btn">
+                                View all
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--why-choose-area  End !-->
+<div class="team-area style-1 py-120 py-sm-60 py-md-80 py-lg-100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-lg-8">
+                <div class="section-title text-center ">
+                    <div class="short-title">
+                        <h3 style="color: #3b1c7e">Gallery</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-lg-4 wow animate__ animate__fadeInUp animate__slower animated activate-animation"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/photo-gallery/shri-varaham-swastik-prasadam.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 wow animate__ animate__fadeInUp animate__slower animated activate-animation"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/photo-gallery/shri-varaham-swastik-food.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 wow animate__ animate__fadeInUp animate__slower animated activate-animation"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/photo-gallery/shri-varaham-swastik-providingfood.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 wow animate__ animate__fadeInUp animate__slower animated activate-animation"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/photo-gallery/shri-varaham-swastik-prize.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 wow animate__ animate__fadeInUp animate__slower animated activate-animation"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/photo-gallery/shri-varaham-swastik-kids-prize.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 wow animate__ animate__fadeInUp animate__slower animated activate-animation"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="team-member-card">
+                    <div class="image">
+                        <img src="images/photo-gallery/shri-varaham-swastik-theertham.jpg" alt="team">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--- Start Footer !-->
+
+
+<?php include 'footer.php';?>
+
+<?php include 'script.php';?>
+
+<?php include 'bottom.php';?>
