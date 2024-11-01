9b2e06df06d67414499065d763ebfbbe85d98429
srarcher <srgithubsr@gmail.com>
2024-11-01 15:52:07 +0530

    header.php added

---
+++ header.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Alagendran Nidhi Limited</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+    <!-- Google font (font-family: 'Dosis', Roboto;) -->
+    <link href="https://fonts.googleapis.com/css?family=Dosis:400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+
+
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Cusom css -->
+
+</head>
+
+<body>
+    <!--<div id="thover"></div>
+<div id="tpopup">
+	<br><br>
+<center><h2>BRANCH OPENING</h2></center><br> 
+<img src="branch-open.jpg" alt=""><br><br>-->
+    <!-- <center><h4><a href="vacancies.html">Click Here For More Information</a></h4></center> -->
+    <!--<div id="tclose">X</div>
+</div> -->
+
+
+    <!-- Main wrapper -->
+    <div class="wrapper" id="wrapper">
+        <!-- Header -->
+        <header id="header" class="jnr__header header--3 header__variation--4 clearfix">
+
+
+            <div class="top-head">
+
+            </div>
+            <!-- Start Header Top Area -->
+            <center>
+                <div class="junior__header__top bg--white">
+                    <div class="container">
+                        <div class="row menu__separator">
+                            <div class="col-md-9 d-none d-lg-block col-lg-9 col-sm-9">
+
+                                <div class="jun__header__top__left">
+                                    <div class="logo">
+                                        <a href="index.php">
+                                            <img src="images/logo/junior-01.gif" alt="logo images">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3 d-none d-lg-block col-lg-3 col-sm-3">
+                                <p style="color:#165407">ONLY FOR THE MEMBERS INFORMATION</p>
+                            </div>
+
+
+
+                            <!-- <div class="col-md-12 col-lg-6 col-sm-12 col-12">
+							<div class="jun__header__top__right">
+								<p><span>Location : </span> No. 95, New Avadi Road,<br>Near M.N Orthopaedic Hospital,<br> Kilpauk, Chennai - 600010.</p>
+								<p><span>Phone :</span> <a href="#"> +91-44-26471384/85<br>044-42875853</a></p>
+							</div>
+						</div> -->
+                        </div>
+                    </div>
+                </div>
+            </center>
+
+
+            <div class="mainmenu__wrapper bg-image--18 sticky__header">
+                <div class="container">
+                    <div class="row d-none d-lg-flex">
+                        <div class="col-sm-4 col-md-2 col-lg-12 order-3 order-lg-2">
+                            <div class="mainmenu__wrap">
+                                <nav class="mainmenu__nav">
+                                    <ul class="mainmenu">
+                                        <li class="drop"><a href="index.php">Home</a></li>
+                                        <li><a href="aboutus.php">About</a></li>
+                                        <li><a href="management.php">Management</a></li>
+                                        <li class="drop" title="JEWEL MORTGAGE LOAN"> <a href="jewel-loans.php"
+                                                class=" giff">Loans</a>
+                                            <ul class="dropdown__menu">
+                                                <li> <a href="jewel-loans.php">Jewel Loans</a></li>
+                                                <li> <a href="jewel-loan-calculator.php">JL Calculator</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="deposits.php" class=" giff">Deposits
+                                                &nbsp;
+                                                <!-- <i class="fa fa-arrow-left"></i> -->
+                                            </a>
+                                            <!-- <ul class="dropdown__menu">
+	<li> <a href="fixed-deposits.html">Fixed Deposits</a></li>
+	<li> <a href="cumulative-deposits.html">Cumulative Deposits</a></li>
+<li> <a href="recurring-deposits.html">Recurring Deposits</a></li>
+	<li> <a href="saving-deposits.html">Savings Deposits</a></li>
+  </ul> -->
+                                        </li>
+                                        <li><a href="anl-culture&core-value.php">ANL Cultures & Core Values</a></li>
+
+                                        <li class="drop" title=""> <a href="careers.php">Careers</a>
+                                            <ul class="dropdown__menu">
+                                                <li><a href="vacancies.php">Vacancies</a></li>
+                                            </ul>
+                                        </li>
+
+                                        <li> <a href="investor-information.php">Investor Information</a></li>
+                                        <li> <a href="faq.php">Faq</a></li>
+                                        <li><a href="testimonials.php">Testimonials</a></li>
+                                        <!-- <li><a href="contact.php">Contact</a></li> -->
+
+										<li class="drop" title=""> <a href="contact.php">Contact</a>
+                                            <ul class="dropdown__menu">
+                                                <li><a href="#">Get in touch</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- Mobile Menu -->
+                    <div class="mobile-menu d-block d-lg-none">
+                        <div class="logo">
+                            <a href="index.php"><img src="images/logo/junior-01.gif" alt="logo"></a>
+                        </div>
+
+
+                    </div>
+                    <!-- Mobile Menu -->
+                </div>
+            </div>
+            <!-- End Mainmenu Area -->
+
+            <style class="INLINE_PEN_STYLESHEET_ID">
+            #thover {
+                position: fixed;
+
+                width: 100%;
+                height: auto;
+                opacity: 0.6;
+            }
+
+            #tpopup {
+                position: fixed;
+                width: 617px;
+                height: 510px;
+                background: #fff;
+                left: 50%;
+                top: 30%;
+                border-radius: 5px;
+                padding: 0px 0;
+                margin-left: -320px;
+                /* width/2 + padding-left */
+                margin-top: -150px;
+                /* height/2 + padding-top */
+                text-align: center;
+                box-shadow: 0 0 10px 0 #0033ff;
+                z-index: 999;
+            }
+
+            #tclose {
+                position: absolute;
+                background: #ff3300;
+                color: white;
+                right: -15px;
+                top: -15px;
+                border-radius: 50%;
+                width: 30px;
+                height: 30px;
+                line-height: 30px;
+                text-align: center;
+                font-size: 12px;
+                font-weight: bold;
+                font-family: 'Arial Black', Arial, sans-serif;
+                cursor: pointer;
+                box-shadow: 0 0 10px 0 #0033ff;
+            }
+
+            .box-border {
+                border: 1px solid #b4ce35;
+                height: 250px;
+                padding: 9px;
+            }
+
+            @media(max-width: 720px) {
+                #tpopup {
+                    width: 100%;
+                }
+
+                .box-border {
+                    height: auto;
+                }
+            }
+            </style>
+
+        </header>
+        <!-- //Header -->
